b9f33fe6ac666da6ec439806612ec8b2b5f3e755
kwakchanhyuk <kwakchanhyuk@naver.com>
2022-11-27 16:30:17 +0900

    푼거 추가

--- 백준/사다리조작.py
+++ 백준/사다리조작.py
@@ -1,5 +1,6 @@
 #N: 세로선, M: 실제로 놓여있는 선, H: 가로선
 N, M, H = map(int,input().split())
+answer = 0
 ladder_list = [[0] * (N+2)  for _ in range(H+1)]
 ladder_dic ={} #행: 열 두개
 for i in range(H+1):
@@ -13,12 +14,18 @@ for ladder in range(M):
     ladder_list[a][b+1] = 1
     ladder_dic[a] += [(b, b+1)]
 
+print(ladder_dic)
+print(ladder_list)
+
 def dfs(row, col, start_col):
+    print("현재노드", row, col)
     cnt = 0
     if row == H+1 and col != start_col:
+        print("마지막에서 돌아가기")
         return -1
     
     elif row == H+1 and col == start_col:
+        print("마지막 일치")
         return 0
     
     if ladder_list[row][col] == 1:
@@ -29,7 +36,10 @@ def dfs(row, col, start_col):
             return dfs(row+1, col-1, start_col)
 
         elif ladder_list[row][col-1] == 1 and ladder_list[row][col+1] == 1:
+            print("양쪽 1")
+            print(ladder_dic)
             for data in ladder_dic[row]:
+                print(data)
                 if col in data:
                     if data[0] == col:
                         return dfs(row+1, col+1, start_col)
@@ -42,63 +52,75 @@ def dfs(row, col, start_col):
             return data
         elif data == -1:
             if ladder_list[row][col+1] == 1 and ladder_list[row][col-1] == 1 or ladder_list[row][col+1] == 1 and ladder_list[row][col-1] == 9 or ladder_list[row][col+1] == 9 and ladder_list[row][col-1] == 9:
+                print("양쪽1이라 위로이동")
                 return data
 
             elif ladder_list[row][col+1] == 0 and ladder_list[row][col-1] == 1:
+                print("오른쪽으로 선긋기")
                 cnt = dfs(row+1,col+1,start_col)
                 if cnt != -1:
                     ladder_list[row][col] = 1
                     ladder_list[row][col+1] = 1
                     ladder_dic[row] += [(col,col+1)]
+                    print(ladder_list)
+                    print(ladder_dic)
                     return cnt+1
                 else:
                     return -1
 
             elif ladder_list[row][col-1] == 1 and ladder_list[row][col-1] == 0:
                 if ladder_list[row][col-1] != 1:
+                    print("왼쪽으로 선긋기")
                     cnt = dfs(row+1,col-1,start_col)
                     if cnt != -1:
                         ladder_list[row][col] = 1
                         ladder_list[row][col-1] = 1
                         ladder_dic[row] += [(col-1,col)]
+                        print(ladder_list)
+                        print(ladder_dic)
                         return cnt+1
                     else:
                         return -1
 
             else:
                 if (start_col - col) < 0:
+                    print("왼쪽으로 선긋기")
                     cnt = dfs(row+1,col-1,start_col)
                     if cnt != -1:
                         ladder_list[row][col] = 1
                         ladder_list[row][col-1] = 1
                         ladder_dic[row] += [(col-1,col)]
+                        print(ladder_list)
                         return cnt+1
                     else:
                         return -1
 
                 else:
+                    print("오른쪽으로 선긋기")
                     cnt = dfs(row+1,col+1,start_col)
                     if cnt != -1:
                         ladder_list[row][col] = 1
                         ladder_list[row][col+1] = 1
                         ladder_dic[row] += [(col,col+1)]
+                        print(ladder_list)
                         return cnt+1
                     else:
                         return -1
+print(ladder_list)
 
-def m():
-    answer = 0
-    for col in range(1,H):
-        cnt = dfs(1, col, col)
-        if cnt == -1:
-            return -1
-        else:
-            answer += cnt
-            if answer > 3:
-                return -1
+for col in range(1,H):
+    print(col,"번째 열")
+    print(ladder_list)
+    cnt = dfs(1, col, col)
+    print(cnt)
+    if cnt == -1:
+        print(-1)
+        break
+    else:
+        answer += cnt
+        if answer > 3:
+            print(-1)
 
-    return answer
-
-print(m())
+print(answer)
     
         
--- 프로그래머스/양궁대회.py
+++ 프로그래머스/양궁대회.py
@@ -15,37 +15,40 @@ def solution(n, info):
     
     for com_r in combinations_with_replacement(score, n):
         info_cnt = 0
+        ryan_cnt = 0
         flag = True
         s_com_r = set(com_r)
         for data in s_com_r:
             #조건에 안맞는 조합들 제거
-            if info[data] >= com_r.count(data) or info[data] + 1 < com_r.count(data):
+            if info[data] + 1 < com_r.count(data):
                 flag = False
                 continue
             
-            #겹치는 점수 info_cnt에 추가
-            if info[data] != 0:
+            #겹치는 점수 info_cnt 혹은 ryan_cnt 에 추가
+            if info[data] != 0 and info[data] < com_r.count(data):
                 info_cnt += data
+
+            elif info[data] >= com_r.count(data):
+                ryan_cnt += data
         
         #어피치가 이기는 경우 제거
-        if info_score - info_cnt >= sum(s_com_r):
+        if info_score - info_cnt >= sum(s_com_r) - ryan_cnt:
             continue
             
-        if flag == True and max < sum(s_com_r) - (info_score - info_cnt):
+        if flag == True and max < (sum(s_com_r) - ryan_cnt) - (info_score - info_cnt):
             max = sum(s_com_r) - (info_score - info_cnt)
             max_tu = com_r 
             
         #최댓값이 같은 것 중에서 점수가 낮은 것을 가지는 것 max_tu 추가
-        if flag == True and max == sum(s_com_r) - (info_score - info_cnt):
+        if flag == True and max == (sum(s_com_r) - ryan_cnt) - (info_score - info_cnt):
             for i in score:
                 if max_tu.count(i) < com_r.count(i):
                     max_tu = com_r
                     break
 
-                elif max_tu.count(i) > com_r.count(i):
-                        
+                elif max_tu.count(i) > com_r.count(i):  
                         break
-    print(max_tu)
+
     for score in max_tu:
         answer[score] += 1
     
