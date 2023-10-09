b61215352973ed6857c65e82c23972e323e2b47e
kwakchanhyuk <kwakchanhyuk@naver.com>
2023-10-09 23:46:38 +0900

    백준 1002 터렛 풀이

--- 백준/1002 터렛.py
+++ 백준/1002 터렛.py
@@ -5,4 +5,26 @@ T = int(input())
 for i in range(T):
     x1, y1, r1, x2, y2, r2 = map(int,sys.stdin.readline().split())
 
-    print(x1, y1, r1, x2, y2)
+    distance = ((x1-x2)**2 + (y1-y2)**2)**0.5
+
+    if distance > r1+r2:
+        print(0)
+
+    elif distance == r1+r2:
+        print(1)
+
+    elif distance < r1+r2:
+        if x1 == x2 and y1 == y2 and r1 == r2:
+            print(-1)
+        elif x1 == x2 and y1 == y2 and r1 != r2:
+            print(0)
+        
+        elif abs(r2 - r1) > distance:
+            print(0)
+
+        elif abs(r2 - r1) == distance:
+            print(1)
+
+        else:
+            print(2) 
+
