bb59d66d27e7cf6c1e810aec5f8ad6ff11d0e9cf
kwakchanhyuk <kwakchanhyuk@naver.com>
2023-10-25 15:11:44 +0900

    백준 2579 풀이

--- 백준/2579 계단오르기.py
+++ 백준/2579 계단오르기.py
@@ -3,41 +3,25 @@ import sys
 input = sys.stdin.readline
 
 stairs = int(input())
-stairs_score = [0]
-
-def dfs(n, score, b):
-    if n < stairs:
-        return
-
-    if n == 0:
-        dfs(n+1, stairs_score[n],False)
-        dfs(n+2, stairs_score[n],True)
-        return
-
-    if b:
-        if n == stairs:
-            b_max[stairs] = max(b_max[stairs], score + stairs_score[-1])
-            return
-        
-        
-
-    else:
-        if n == stairs:
-            a_max[stairs] = max(b_max[stairs], score + stairs_score[-1])
-            return
-
-
-
-
-        stairs_score[n]
-
-#a -> 다음 계단을 밟을 수 있는 상태의 각 계단에서의 최대값: +1이 가능
-#b -> 다음 계단을 밟을 수 없는 상태의 각 계단에서의 최대값: +2가 불가능
-a_max = [0 for _ in range(stairs+1)]
-b_max = [0 for _ in range(stairs+1)]
-max_li = []
+stairs_score = []
+dp = [0 for _ in range(stairs)]
 
 for _ in range(stairs):
     stairs_score.append(int(input()))
 
-dfs(0, 0, True)
+if stairs <= 2:
+    print(sum(stairs_score))
+    exit()
+
+dp[0] = stairs_score[0]
+dp[1] = stairs_score[0]+stairs_score[1]
+dp[2] = max(stairs_score[0]+stairs_score[2], stairs_score[1]+stairs_score[2])
+
+if stairs == 3:
+    print(dp[2])
+
+else:
+    for i in range(3, stairs):
+        dp[i] = max(dp[i-2], dp[i-3]+stairs_score[i-1]) + stairs_score[i]
+
+    print(dp[-1])
